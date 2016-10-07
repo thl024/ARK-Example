7c462969afd3795f477d08a84743c08c3771d53b
timothylam <lamtimothy1996@gmail.com>
2016-10-07 15:28:32 -0700

    Reorganized and added gitignore

--- ARKExample/SurveyTask.swift
+++ ARKExample/SurveyTask.swift
@@ -13,8 +13,8 @@ public var SurveyTask: ORKOrderedTask {
     var steps = [ORKStep]()
     
     let instructionStep = ORKInstructionStep(identifier: "IntroStep")
-    instructionStep.title = "The Questions Three"
-    instructionStep.text = "Who would cross the Bridge of Death must answer me these questions three, ere the other side they see."
+    instructionStep.title = "Sample Survey"
+    instructionStep.text = "Complete this survey for us to know more about you!"
     steps += [instructionStep]
     
     let nameAnswerFormat = ORKTextAnswerFormat(maximumLength: 20)
@@ -23,7 +23,16 @@ public var SurveyTask: ORKOrderedTask {
     let nameQuestionStep = ORKQuestionStep(identifier: "QuestionStep", title: nameQuestionStepTitle, answer: nameAnswerFormat)
     steps += [nameQuestionStep]
     
-    
+    let foodQuestionStepTitle = "Out of the four, which food do you prefer?"
+    let textChoices = [
+        ORKTextChoice(text: "Sushi", value: 0 as NSCoding & NSCopying & NSObjectProtocol),
+        ORKTextChoice(text: "Taco", value: 1 as NSCoding & NSCopying & NSObjectProtocol),
+        ORKTextChoice(text: "Burger", value: 2 as NSCoding & NSCopying & NSObjectProtocol),
+        ORKTextChoice(text: "Pasta", value: 3 as NSCoding & NSCopying & NSObjectProtocol)
+    ]
+    let foodAnswerFormat: ORKTextChoiceAnswerFormat = ORKAnswerFormat.choiceAnswerFormat(with: .singleChoice, textChoices: textChoices)
+    let foodQuestionStep = ORKQuestionStep(identifier: "TextChoiceQuestionStep", title: foodQuestionStepTitle, answer: foodAnswerFormat)
+    steps += [foodQuestionStep]
     
     return ORKOrderedTask(identifier: "SurveyTask", steps: steps)
 
